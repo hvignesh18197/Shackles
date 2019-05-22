dca02574ba68d75e3e1f277597fea9a6e43571c8
viky <hvignesh18197@gmail.com>
2019-05-22 23:12:16 +0530

    new

--- app/src/main/java/in/hawkshaw/shackles/QuestionsActivity.java
+++ app/src/main/java/in/hawkshaw/shackles/QuestionsActivity.java
@@ -3,12 +3,11 @@ package in.hawkshaw.shackles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class QuestionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_questions);
     }
-    public void On
 }
